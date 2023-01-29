70b503d2b2b6c99d3a600189c822232afb3be7dc
atas-parit <123853160+atas-parit@users.noreply.github.com>
2023-01-29 11:38:42 +0700

    Create up.py

---
+++ up.py
@@ -0,0 +1,13 @@
+import time
+from playwright.sync_api import sync_playwright
+
+def run(playwright):
+    chromium = playwright.chromium
+    browser = chromium.launch()
+    page = browser.new_page()
+    page.goto("https://argh.pages.dev/")
+    time.sleep(300)
+    browser.close()
+
+with sync_playwright() as playwright:
+    run(playwright)
